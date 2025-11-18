141518b282bb1847afba7b16590d9040c6654560
ilhamkhalik05 <believerrr05@gmail.com>
2025-11-18 09:51:20 +0700

    feat: Create isValidChoice utils and display main module options

--- src/main.c
+++ src/main.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void header();
+bool isValidChoice(int start, int end, int choice);
 
 int main()
 {
+    int selectedMainModule;
+
     header();
+
+    do
+    {
+        printf("\n");
+
+        printf("Pilih modul yang tersedia:\n");
+        printf("1. Produk\n");
+        printf("2. Transaksi\n");
+
+        printf("Pilih berdasarkan angka (1-2): ");
+        scanf("%d", &selectedMainModule);
+    } while (!isValidChoice(1, 2, selectedMainModule));
+
+    printf("\nPASSED\n");
     return 0;
 }
 
@@ -20,4 +38,26 @@ void header()
     printf("=============================================================\n");
     printf("========== Program Simulasi Manajemen Toko Sembako ==========\n");
     printf("=============================================================\n");
+}
+
+/**
+ * Checks if the given choice is valid
+ * within the range of start and end
+ *
+ * @param start the start of the range
+ * @param end the end of the range
+ * @param choice the choice to be validated
+ * @return true if the choice is valid, false otherwise
+ */
+bool isValidChoice(int start, int end, int choice)
+{
+    for (int i = start; i <= end; i++)
+    {
+        if (i == choice)
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
